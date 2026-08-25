ccb8fadb326a07d80eab163f63a4e659ba42bc78
AyaanAhmed99 <ayaanpatel2006@gmail.com>
2026-08-26 00:12:31 +0530

    add role-based access control middleware

---
+++ backend/src/middleware/authorize.middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { JwtPayload } from "../utils/jwt";
+
+export function authorize(...allowedRoles: JwtPayload["role"][]) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: "Not authenticated",
+      });
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "You do not have permission to access this resource",
+      });
+    }
+
+    next();
+  };
+}
